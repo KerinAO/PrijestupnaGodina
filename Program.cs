8c5ab82aabadaded4dea9e52d91398d124365e0a
KerinAO <90246117+KerinAO@users.noreply.github.com>
2022-02-01 12:58:51 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ponavljanje
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 1900; i <= 2100; i++)
+            {
+                if ((i % 400 == 0 || i % 100 != 0) && (i % 4 == 0))
+                {
+                    Console.WriteLine("Prijestupna godina: " + i + "\n");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
